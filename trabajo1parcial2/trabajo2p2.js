89e409728c6c60430dad122f22481b27bbeddd7b
Sebaslv1503 <lassosebastian66@gmail.com>
2025-01-06 10:43:48 -0500

    trabajo en clase calcular promedio con validaciones

--- trabajo1parcial2/trabajo2p2.js
+++ trabajo1parcial2/trabajo2p2.js
@@ -1,13 +1,13 @@
 
   
-  const calcularPromedio = (num1, num2) => {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+  function calcularPromedio(num1, num2, num3) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number'|| typeof num3 !== 'number') {
       throw new Error('Ambos parámetros deben ser números.');
     }
-    return (num1 + num2) / 2;
+    return (num1 + num2+num3) / 3;
   };
   
-  const determinarMayor = (num1, num2) => {
+  function determinarMayor (num1, num2) {
     if (typeof num1 !== 'number' || typeof num2 !== 'number') {
       throw new Error('Ambos parámetros deben ser números.');
     }
@@ -26,13 +26,13 @@
       
       const num1 = 10;
       const num2 = 20;
+      const num3 = 30;
   
       
-      const promedio = calcularPromedio(num1, num2);
+      const promedio = calcularPromedio(num1, num2, num3);
       const mayor = determinarMayor(num1, num2);
       const esNum1Par = esPar(num1);
   
-      // Mostrar resultados en la consola
       console.log(`Promedio: ${promedio}`);
       console.log(`Mayor: ${mayor}`);
       console.log(`¿El número ${num1} es par?: ${esNum1Par}`);
