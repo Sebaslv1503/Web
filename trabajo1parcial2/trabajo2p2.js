778df48f29c0cc7afe2fa9b84a31a84c9e88a32f
Sebaslv1503 <lassosebastian66@gmail.com>
2025-01-06 10:40:03 -0500

    .

---
+++ trabajo1parcial2/trabajo2p2.js
@@ -0,0 +1,43 @@
+
+  
+  const calcularPromedio = (num1, num2) => {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      throw new Error('Ambos parámetros deben ser números.');
+    }
+    return (num1 + num2) / 2;
+  };
+  
+  const determinarMayor = (num1, num2) => {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      throw new Error('Ambos parámetros deben ser números.');
+    }
+    return num1 > num2 ? num1 : num2;
+  };
+
+  const esPar = (numero) => {
+    if (typeof numero !== 'number') {
+      throw new Error('El parámetro debe ser un número.');
+    }
+    return numero % 2 === 0;
+  };
+  
+  (() => {
+    try {
+      
+      const num1 = 10;
+      const num2 = 20;
+  
+      
+      const promedio = calcularPromedio(num1, num2);
+      const mayor = determinarMayor(num1, num2);
+      const esNum1Par = esPar(num1);
+  
+      // Mostrar resultados en la consola
+      console.log(`Promedio: ${promedio}`);
+      console.log(`Mayor: ${mayor}`);
+      console.log(`¿El número ${num1} es par?: ${esNum1Par}`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })();
+  
